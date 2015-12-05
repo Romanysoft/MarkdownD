fe591b61e6804f3b036464edbbc894fb88ed5eb1
iansun <lauer3912@gmail.com>
2015-12-05 00:07:03 -0800

    add footer.js

---
+++ js/views/footer.js
@@ -0,0 +1,9 @@
+(function(){
+    var htmlContent ="            <span class=\"site-footer-owner\"><a href=\"https://github.com/Romanysoft/MarkdownD\">MarkdownD</a> is developed by <a href=\"http://www.romanysoft.com\">Romanysoft LAB.</a></span>";
+    htmlContent += "            <span class=\"site-footer-credits\">Copyright © 2015 Romanysoft LAB.</span>";
+    
+
+    $(document).ready(function(){
+        $('.site-footer').html(htmlContent);
+    })
+})();
