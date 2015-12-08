dad99364150b0201f0c1b65d0e982908186cfdcf
iansun <lauer3912@gmail.com>
2015-12-07 22:54:41 -0800

    更新google分析及发布页面

--- js/google-analytics.js
+++ js/google-analytics.js
@@ -1,8 +1,16 @@
-(function () {
+(function() {
+
     var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
     document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-    try {
-        var pageTracker = _gat._getTracker("UA-43238484-4");
-        pageTracker._trackPageview();
-    } catch (err) {}
+
+    $(document).ready(function() {
+        try {
+            var pageTracker = _gat._getTracker("UA-43238484-4");
+            pageTracker._trackPageview();
+        } catch (err) {
+            console.error(err);
+        }
+    })
+
+
 })();
