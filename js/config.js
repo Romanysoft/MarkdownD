88efc8c2350b08fae2c424853527c4e77784a9ea
iansun <lauer3912@gmail.com>
2016-01-05 18:52:52 -0800

    更新

--- js/config.js
+++ js/config.js
@@ -6,6 +6,6 @@
         googleUA:"UA-43238484-4",
         gitHome:"https://github.com/Romanysoft/MarkdownD",
         romanysoftHome:"http://www.romanysoft.com",
-        copyright:"Copyright © 2015 Romanysoft LAB."
+        copyright:"Copyright © 2016 Romanysoft LAB."
     }
 })();
