37cb681821bfd5e29799c33537acebc7b54e8fa3
iansun <lauer3912@gmail.com>
2016-04-19 21:06:01 +0800

    更新

--- js/config.js
+++ js/config.js
@@ -5,11 +5,11 @@
         documentTitle:"MarkdownD By Romanysoft",
         supportPlatforms:["Mac","Windows","Linux"],
         googleUA:"UA-43238484-4",
-        gitHome:"https://github.com/Romanysoft/MarkdownD",
-        reportIssueUrl:"https://github.com/Romanysoft/MarkdownD/issues",
-        changeLogUrl:"https://github.com/Romanysoft/MarkdownD/wiki/Changelog",
-        wikiUrl:"https://github.com/Romanysoft/MarkdownD/wiki",
-        romanysoftHome:"http://www.romanysoft.com",
+        gitHome:"//github.com/Romanysoft/MarkdownD",
+        reportIssueUrl:"//github.com/Romanysoft/MarkdownD/issues",
+        changeLogUrl:"//github.com/Romanysoft/MarkdownD/wiki/Changelog",
+        wikiUrl:"//github.com/Romanysoft/MarkdownD/wiki",
+        romanysoftHome:"//www.romanysoft.com",
         copyright:"Copyright © " + (new Date()).getFullYear() + " Romanysoft LAB."
     }
 })();
