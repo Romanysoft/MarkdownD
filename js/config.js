d0238db7999ccb7d033e2bd31450df8cb7e6d184
iansun <lauer3912@gmail.com>
2016-04-20 13:55:27 +0800

    使用新的google跟踪号

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@
         appID:"com.romanysoft.app.macos.MarkdownD",
         documentTitle:"MarkdownD By Romanysoft",
         supportPlatforms:["Mac","Windows","Linux"],
-        googleUA:"UA-43238484-4",
+        googleUA:"UA-76676167-2",
         gitHome:"//github.com/Romanysoft/MarkdownD",
         reportIssueUrl:"//github.com/Romanysoft/MarkdownD/issues",
         changeLogUrl:"//github.com/Romanysoft/MarkdownD/wiki/Changelog",
